cc59d6ad6ee3c00f969a4ff0eedbff7a2912eaf6
Jaachimma Onyenze <onyenze.jaachimma@gmail.com>
2018-10-19 07:32:38 +0100

    updated build with test files

--- build/controllers/productController.js
+++ build/controllers/productController.js
@@ -11,42 +11,44 @@ var _require = require('express-validator/check'),
     validationResult = _require.validationResult;
 
 exports.getAll = function (req, res) {
-	return res.status(200).json(_products2.default);
+  return res.status(200).json(_products2.default);
 };
 
 exports.getProduct = function (req, res) {
-	var productId = req.params.productId;
-	var requiredProduct = null;
+  var productId = req.params.productId;
+  var requiredProduct = null;
 
-	_products2.default.forEach(function (product) {
-		if (product.id == productId) {
-			requiredProduct = product;
-		};
-	});
+  _products2.default.forEach(function (product) {
+    if (product.id == productId) {
+      requiredProduct = product;
+    }
+  });
 
-	return res.status(200).json(requiredProduct);
+  return res.status(200).json(requiredProduct);
 };
 
 exports.addProduct = [body('name', 'product name is required').isLength({ min: 1 }).trim(), body('category', 'product category is required').isLength({ min: 1 }), body('qty_left', 'qty_left should be a number').isNumeric(), body('qty_sold', 'qty_sold should be a number').isNumeric(), body('price', 'price should be a number').isNumeric(), body('min_qty', 'min_qty should be a number').isNumeric(), function (req, res) {
-	var errors = validationResult(req);
+  var errors = validationResult(req);
 
-	if (!errors.isEmpty) {
-		res.status(422).send('problem with data supplied');
-	};
+  if (!errors.isEmpty) {
+    res.status(422).send('problem with data supplied');
+  }
 
-	var _req$body = req.body,
-	    name = _req$body.name,
-	    category = _req$body.category,
-	    qty_left = _req$body.qty_left,
-	    qty_sold = _req$body.qty_sold,
-	    price = _req$body.price,
-	    min_qty = _req$body.min_qty;
+  var _req$body = req.body,
+      name = _req$body.name,
+      category = _req$body.category,
+      qty_left = _req$body.qty_left,
+      qty_sold = _req$body.qty_sold,
+      price = _req$body.price,
+      min_qty = _req$body.min_qty;
 
-	var id = _products2.default.length + 1;
+  var id = _products2.default.length + 1;
 
-	var newProduct = { id: id, name: name, category: category, qty_left: qty_left, qty_sold: qty_sold, price: price, min_qty: min_qty };
+  var newProduct = {
+    id: id, name: name, category: category, qty_left: qty_left, qty_sold: qty_sold, price: price, min_qty: min_qty
+  };
 
-	_products2.default.push(newProduct);
-	return res.status(201).json({ created: newProduct, success: true });
+  _products2.default.push(newProduct);
+  return res.status(201).json({ created: newProduct, success: true });
 }];
 //# sourceMappingURL=productController.js.map
--- build/test/controllers/productController.js
+++ build/test/controllers/productController.js
@@ -1,10 +1,37 @@
 'use strict';
 
-var getAll = require('../../controllers/productController').getAll;
-var expect = require('chai').expect;
+var _productController = require('../../controllers/productController');
 
-describe('getAll', function () {
+var _productController2 = _interopRequireDefault(_productController);
 
-	context('');
+var _app = require('../../../app');
+
+var _app2 = _interopRequireDefault(_app);
+
+var _chai = require('chai');
+
+var _chai2 = _interopRequireDefault(_chai);
+
+var _chaiHttp = require('chai-http');
+
+var _chaiHttp2 = _interopRequireDefault(_chaiHttp);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_chai2.default.use(_chaiHttp2.default);
+
+var expect = _chai2.default.expect;
+var getAll = _productController2.default.getAll;
+
+describe('products', function () {
+
+	it('Should return products', function (done) {
+		_chai2.default.request(_app2.default).get('/api/v1/products').end(function (err, res) {
+			expect(err).to.be.null;
+			expect(res).to.have.status(200);
+			expect(res).to.be.an('object');
+			done();
+		});
+	});
 });
 //# sourceMappingURL=productController.js.map
