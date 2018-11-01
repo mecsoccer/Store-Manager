3846abdc83810d38abe336199187ab935e22820a
Jaachimma Onyenze <onyenze.jaachimma@gmail.com>
2018-11-01 23:55:20 +0100

    deploy

--- server/models/database.js
+++ server/models/database.js
@@ -2,8 +2,11 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 
 const connectionString = process.env.DATABASE_URL || 'postgres://postgres:06384579hf@localhost:5432/postgres';
+let ssl = false;
+
+if (process.env.NODE_ENV) ssl = true;
 
 const { Pool } = pg;
-const pool = new Pool({ connectionString });
+const pool = new Pool({ connectionString, ssl });
 
 export default pool;
