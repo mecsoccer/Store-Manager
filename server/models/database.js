bb930e4b3b8b0063f404f2814d11a44739dac8a5
Jaachimma Onyenze <onyenze.jaachimma@gmail.com>
2018-11-01 18:18:33 +0100

    deploy

--- server/models/database.js
+++ server/models/database.js
@@ -2,6 +2,7 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
 const { Pool } = pg;
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({ connectionString });
