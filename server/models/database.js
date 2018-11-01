9a509f8b1d18787d84333fe66f51b2e964f49b25
Jaachimma Onyenze <onyenze.jaachimma@gmail.com>
2018-11-01 23:06:01 +0100

    deploy

--- server/models/database.js
+++ server/models/database.js
@@ -3,8 +3,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+let connectionString;
+
+if (process.env.NODE_ENV === 'development') {
+  connectionString = process.env.DATABASE_URL_DEV;
+} else if (process.env.NODE_ENV === 'production') {
+  connectionString = process.env.DATABASE_URL;
+}
+
 const { Pool } = pg;
-const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({ connectionString });
 
 export default pool;
