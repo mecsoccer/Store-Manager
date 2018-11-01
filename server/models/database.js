eca731301ec1eece084b42d57b09e6de66e36007
Jaachimma Onyenze <onyenze.jaachimma@gmail.com>
2018-11-01 23:26:28 +0100

    deploy

--- server/models/database.js
+++ server/models/database.js
@@ -1,15 +1,7 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
 
-dotenv.config();
-
-let connectionString;
-
-if (process.env.NODE_ENV === 'development') {
-  connectionString = process.env.DATABASE_URL_DEV;
-} else if (process.env.NODE_ENV === 'production') {
-  connectionString = process.env.DATABASE_URL;
-}
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:06384579hf@localhost:5432/postgres';
 
 const { Pool } = pg;
 const pool = new Pool({ connectionString });
