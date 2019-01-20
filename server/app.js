57eef90d9656916d29e7eefeafbc9c848868c6fc
Jaachimma Onyenze <onyenze.jaachimma@gmail.com>
2019-01-20 04:03:17 +0100

    fix CORS

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'UI')));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
+  res.header('Access-Control-Allow-Origin', 'https://mecsoccer.github.io');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-type, Authorization, Accept');
   res.header('Access-Control-Allow-Methods', 'POST, OPTIONS, GET, PUT, DELETE');
